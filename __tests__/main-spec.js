de788aff9c8844b4e83b99488d14138e6ce96cbb
guoyangnice <yangjienice@126.com>
2019-08-22 09:59:46 +0800

    提交新代码

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,21 @@
-const add = require('../main');
+const createMutiplyTable1 = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it('should return multiply table from 2 to 4 when call createMultiplyTable given 2 and 4', () => {
+    expect(createMutiplyTable1(2, 4))
+        .toBe("2*2=4\t\n2*3=6\t3*3=9\t\n2*4=8\t3*4=12\t4*4=16");
 });
+
+// it('should return false', () => {
+//     expect(createMultiplyTable('2', '4'))
+//         .toBe(false);
+// });
+
+// it('should return false', () => {
+//     expect(createMultiplyTable(0, '4'))
+//         .toBe(false);
+// });
+
+// it('should return false', () => {
+//     expect(createMultiplyTable(1001, 1001))
+//         .toBe(false);
+// });
--- main.js
+++ main.js
@@ -1,5 +1,64 @@
-function add (left, right) {
-    return left + right;
+// function createMultiplyTable(start, end) {
+//     if (isValid(start, end)) {
+//         let result = ""
+//         for (var i = start; i <= end; i++) {
+//             var arr = "";
+//             for (var j = start; j <= i; j++) {
+//                 arr += (j + "*" + i + "=" + j * i + " ");
+//             }
+//             result += (arr + "\n");
+//         }
+//     }
+//     return result;
+// }
+
+function createMutiplyTable1(start, end){
+    if(isValid(start,end)){
+        return getLine(start,end);
+    }
+    return null;
 }
 
-module.exports = add;
+//检验合法性
+function isValid(start, end) {
+    if (Number.isInteger(start) && Number.isInteger(end)) {
+        if (start < 1001 && end < 1001) {
+            if (start <= end) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+//得到每一行,并拼接
+function getLine(start,end) {
+    let result = "";
+    for(let i = start; i <= end ;i++){
+        const seperate = start === end ? "" : "\n";
+        result += getItem(i,end)+seperate;
+    }
+    return result;
+}
+
+//得到每个条目
+function getItem(start, end) {
+    let result = "";
+    for (let i = start; i <= end; i++) {
+        const seperate = start === end ? "" : "\t";
+        result += getEveryResult(start,i) + seperate;
+    }
+    return result;
+}
+
+//计算每个结果
+function getEveryResult(first,second){
+    return `${first} * ${second} = ${first*second}`;
+}
+
+module.exports = createMutiplyTable1;
